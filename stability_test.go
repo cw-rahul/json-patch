dcedc04da262299f46324b9b064c8a73803d4bd3
Igor Gubernat <igorgubernat@gmail.com>
2019-12-02 13:22:00 +0200

    RMM-51504: Renaming

--- stability_test.go
+++ stability_test.go
@@ -6,13 +6,15 @@ import (
 
 var N = 100 // number of patch apply iterations
 
-func TestOrdering(t *testing.T) {
+// After applying patches we should always get the same result
+// not just semantically, but also byte-by-byte.
+func TestStability(t *testing.T) {
 	reference, err := apply(original, patches)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// apply patches N times and make sure that we get the same result each time
+	// apply patches N times and verify that we get the same result each time
 	for i := 0; i < N; i++ {
 		p, err := apply(original, patches)
 		if err != nil {
